c7e1e1513971d936efdac4949d4093d86284f669
vincenttrinh9098 <vincenttrinh9098@gmail.com>
2026-04-24 12:50:23 -0700

    Finished barebones of home/discovery page

--- cafe-finder-frontend/src/pages/discovery/Discovery.jsx
+++ cafe-finder-frontend/src/pages/discovery/Discovery.jsx
@@ -26,7 +26,20 @@ export function Discovery() {
     ]
   };
 
-  const stores = [store1, store1, store1, store1, store1];
+  const store2 = {
+    name: "Greem Tea",
+    image: storeTestImg,
+    rating: 9.9,
+    distance: "0.3mi",
+    attributes: [
+      "Low noise",
+      "Low foot traffic",
+      "Moderate seating capacity",
+      "Parking lot"
+    ]
+  };
+
+  const stores = [store1, store2, store1, store2, store1];
 
   const handleSelect = (item) => {
     setSearch(item);
@@ -63,8 +76,13 @@ export function Discovery() {
       {/* Discovery page top rated container */}
       <div className={styles.topRatedContainer}>
         {stores.map((store) => (
-          <div key={store.name} className={styles.topRatedCard}>
-
+            <Link
+              key={store.name}
+              //to={`/place/${store.name}`}   // dynamic route
+              to={'/place'}
+              className={styles.cardLink}   // remove default link styles
+            >
+            <div className={styles.topRatedCard}>
             {/* Top half: image */}
             <div className={styles.topRatedCardImageWrapper}>
               <img
@@ -102,6 +120,7 @@ export function Discovery() {
 
 
           </div>
+          </Link>
         ))}
       </div>
 
