a8c2fce685769b701ec3f6a1bbeef771a6c0ef52
vincenttrinh9098 <vincenttrinh9098@gmail.com>
2026-04-28 01:56:41 -0700

    working api CALLS

--- cafe-finder-frontend/src/pages/place-details/Place.jsx
+++ cafe-finder-frontend/src/pages/place-details/Place.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import styles from './Place.module.css';
-
 import { PlaceHeader } from './TabPlacesContent/PlaceHeader.jsx';
 import { TabBar } from './TabBar.jsx';
 import { TabContent } from './TabContent.jsx';
 
 export function Place() {
   const [activeTab, setActiveTab] = useState("info");
+  const { state } = useLocation()
+  const place = state?.place;
+  console.log("place:", place);
 
   return (
     <div className={styles.placePage}>
-      <PlaceHeader />
+      <PlaceHeader place = {place}/>
 
       <TabBar activeTab={activeTab} setActiveTab={setActiveTab} />
 
